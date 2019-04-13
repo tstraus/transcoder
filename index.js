80003d823ed55243b2bfcc95ddb720b17188c2fb
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2019-04-12 20:54:33 -0400

    figure out presets

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ app.get('/', (req, res) => res.send('Transcoder'))
 
 app.listen(port, () => console.log(`Transcoder listening on port ${port}`))
 
-hbjs.spawn({ input: 'something.avi', output: 'something.m4v' })
+hbjs.spawn({
+        'input': 'veep.mkv',
+        'output': 'output.mp4',
+        'encoder': 'x265_10bit',
+        'encoder-preset': 'fast',
+        'encoder-profile': 'auto',
+        'quality': 18.0,
+        'aencoder': 'copy'
+    })
     .on('error', err => {
         // invalid user input, no video found etc
         console.log('ERROR: ' + err)
+    })
+    .on('eError', err => {
+        // encoding error
+        console.log('ENCODING ERRR: ' + err)
     })
     .on('progress', progress => {
         console.log(
