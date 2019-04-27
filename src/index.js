bc109727d932eb1abba925619bb9e0b2ddb02fb0
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2019-04-27 00:04:17 -0400

    add sample file and count for multiple output file s

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const hbjs = require('handbrake-js')
 const app = express()
 const port = 3000
 
+var count = 0
+
 app.use(express.json())
 app.use('/', express.static('public'))
 
 app.post("/encode", (request, response) => {
     console.log(request.body)
 
+    count += 1
+
     hbjs.spawn({
             'input': request.body.file,
-            'output': './samples/out.mp4',
+            'output': './samples/out' + count + '.mp4',
             //'encoder': 'x265_10bit',
             'encoder': 'x264',
             'encoder-preset': 'fast',
