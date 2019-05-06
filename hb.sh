575ea6d5659313cafe6c906b752958d4d6bd613c
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2019-05-05 20:24:45 -0400

    receiving json from website

--- hb.sh
+++ hb.sh
@@ -2,8 +2,8 @@
 
 input='samples/in.mp4'
 id=$1
-log='samples/'$id'.log'
-output='samples/'$id'.mp4'
+log='samples/out_'$id'.log'
+output='samples/out_'$id'.mp4'
 
 HandBrakeCLI --json --input=$input --output=$output --encoder=x264 --encoder-preset=fast --encoder-profile=auto --quality=18.0 --aencoder=copy 1> $log 2> /dev/null
 
--- src/transcoder.cr
+++ src/transcoder.cr
@@ -1,4 +1,18 @@
-p = Process.new("./hb.sh", args: ["1"])
-p.wait()
+require "kemal"
 
-puts "done"
+module TRANSCODER
+    get "/" do |env|
+        env.redirect "index.html"
+    end
+
+    post "/encode" do |env|
+        puts env.params.json
+    end
+
+    p = Process.new("./hb.sh", args: ["1"])
+    p.wait()
+    
+    puts "done"
+
+    Kemal.run
+end
